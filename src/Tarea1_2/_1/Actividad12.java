f2814b276ea7bbff037cd93a9e7371e4de353009
silcaclei <silviacachonleiva@gmail.com>
2025-09-17 18:41:18 +0200

    v3

--- src/Tarea1_2/_1/Actividad12.java
+++ src/Tarea1_2/_1/Actividad12.java
@@ -1,5 +1,7 @@
 package Tarea1_2._1;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -14,11 +16,31 @@ public class Actividad12 {
         Path file2 = Path.of("2.txt");
         Path mixed = Path.of("fusion.txt");
 
-        try {
-            Files.newBufferedReader(file1, StandardCharsets.UTF_8);
-            Files.newBufferedReader(file2, StandardCharsets.UTF_8);
-            Files.newBufferedWriter(mixed,StandardCharsets.UTF_8, StandardOpenOption.APPEND);
+        try (BufferedReader brFile1 = Files.newBufferedReader(file1, StandardCharsets.UTF_8);
+             BufferedReader brFile2 = Files.newBufferedReader(file2, StandardCharsets.UTF_8);
+             BufferedWriter bw = Files.newBufferedWriter(mixed,StandardCharsets.UTF_8, StandardOpenOption.CREATE);){
 
+            String lineaFile1;
+            String lineaFile2 = null;
+            int numLineasFile1 = 0;
+            int numLineasFile2 = 0;
+
+            while ((lineaFile1 = brFile1.readLine())!= null ||
+                    (lineaFile2 = brFile2.readLine()) != null){
+
+                if(lineaFile1 != null){
+                    bw.write(lineaFile1 + System.lineSeparator());
+                    numLineasFile1++;
+                }
+                if(lineaFile2 != null) {
+                    bw.write(lineaFile2 + System.lineSeparator());
+                    numLineasFile2++;
+                }
+
+            }
+            System.out.println("Lineas file1: " + numLineasFile1);
+            System.out.println("Lineas file2: " +numLineasFile2);
+            System.out.println("Total lineas: " +(numLineasFile1+numLineasFile2));
 
         } catch (IOException e) {
             throw new RuntimeException(e);
